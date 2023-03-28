f7ececc83bb9a3b65d8be584d8aec242226ba1dd
EdAlita <eulinbriseno@gmail.com>
2023-03-28 10:02:43 +0200

    Solvig some issues with the .config file adding viewing of the array of images

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 from glob import glob
+import cv2
 
 #Open config file to get the ṕath of our images.
 
@@ -15,11 +16,23 @@ Path to training images
 .gitignore will not let this file upload to this file to our file structure
 
 """
+
+#Open file to obtain local variables
 with open('main.cfg', 'r') as file:
     test = file.readline().rstrip()
     training = file.readline().rstrip()
 
 #Creating data sets of all the images.
 
+# Make empty list
+
 ds_ts = [ cv2.imread(fn, cv2.IMREAD_COLOR) for fn in glob(test) ]
 ds_tr = [ cv2.imread(fn, cv2.IMREAD_COLOR) for fn in glob(training) ]
+
+print("The list length of the test is "+str(len(ds_ts)))
+print("The list length of the test is "+str(len(ds_tr)))
+
+
+
+cv2.imshow('sample image',ds_ts[3])
+cv2.waitKey()
